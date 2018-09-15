62c6477308b4e94f835a6513390810a89eceec9c
kanishka kumar <vinoy111@gmail.com>
2018-09-15 08:34:39 +0530

    bugs rectified

--- js/post.js
+++ js/post.js
@@ -4,7 +4,7 @@ function likeFn() {
     var template = count + ' person likes this!';
     count++;
     document.getElementById('likePara').innerHTML=template;
-    document.getElementsByClassName('likeButton')[0].innerHTML="Liked!";
+    document.getElementsByClassName('likeButton')[0].innerHTML='<i class="fa fa-thumbs-up"></i>'+"Liked!";
 }
 //function to show all comments with the latest comment coming first
 function allCommt(id) {
@@ -22,7 +22,7 @@ var count1=0;
 function editFunction() {
     if(count1===0){
         document.getElementById("blogBody").contentEditable = true;
-        document.getElementsByClassName('editButton')[0].innerHTML="Save";
+        document.getElementsByClassName('editButton')[0].innerHTML="Save"+'<i class="fa fa-save"></i>';
         count1++;
     }else{
         document.getElementById("blogBody").contentEditable = false; 
